73a046d5a33039ae6ab0f5c201adc6fdc8ef96f6
uchexm <109084069+uchexm@users.noreply.github.com>
2023-08-23 13:25:56 +0200

    I O cycle

---
+++ Coursera MEAN/demo/modules/model.js
@@ -0,0 +1,13 @@
+// require, exports, modules, __dirname, __filename
+
+// console.log(__dirname);
+// console.log(__filename);
+
+let names = ["Alex", "John", "Rody"];
+let scores = [29, 77, 99, 100];
+
+exports = names;
+module.exports.names = names;
+module.exports.scores = scores;
+
+console.log(module);
