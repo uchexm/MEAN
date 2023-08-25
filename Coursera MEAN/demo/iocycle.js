43e1bc9465557cded06bf84e86ea3801bff4d6f2
uchexm <109084069+uchexm@users.noreply.github.com>
2023-08-25 12:20:14 +0200

    initial

---
+++ Coursera MEAN/demo/iocycle.js
@@ -0,0 +1,22 @@
+let fs = require("fs");
+
+let total = 0;
+
+// I/O Cycle
+fs.readFile("./index.js", () => {
+  setTimeout(print1, 0);
+  setImmediate(print2); //set immediater gets priority in the I O cycle
+});
+
+let n = 1;
+
+function print1() {
+  total = n * 10;
+  console.log("The total 1 is:", total);
+  n++;
+}
+
+function print2() {
+  total = n * 10;
+  console.log("The total 2 is:", total);
+}
