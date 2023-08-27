fc69a5e4abd7b4d2a9080046e7bd36bb1bfb5209
uchexm <109084069+uchexm@users.noreply.github.com>
2023-08-27 08:50:35 +0200

    Routing nodejs

--- Coursera MEAN/demo/app.js
+++ Coursera MEAN/demo/app.js
@@ -3,18 +3,22 @@ const server = http.createServer((request, response) => {
   response.writeHead(200, "content-type", "text/html");
 
   if (request.url === "/get") {
+    response.write("You reached the GET page");
     if (request.method === "GET") {
       response.end("GET");
     }
   } else if (request.url === "/post") {
+    response.write("You reached the POST page");
     if (request.method === "POST") {
       response.end("POST");
     }
   } else if (request.url === "/put") {
+    response.write("You reached the PUT page");
     if (request.method === "PUT") {
       response.end("PUT");
     }
   } else if (request.url === "/delete") {
+    response.write("You reached the DELETE page");
     if (request.method === "DELETE") {
       response.end("DELETE");
     }
