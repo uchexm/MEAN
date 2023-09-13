5a2f48505fc7e29ed078c9f09822de168c16b20c
uchexm <109084069+uchexm@users.noreply.github.com>
2023-09-13 11:28:45 +0200

    edit page

--- Coursera MEAN/Back-end/controllers/providers.js
+++ Coursera MEAN/Back-end/controllers/providers.js
@@ -30,9 +30,70 @@ module.exports.edit = function (req, res) {
 module.exports.update = function (req, res) {
   let id = req.params.id;
   let provider = providers.find((provider) => provider.id == id);
+
+  provider.firstname = req.body.firstname;
+  provider.lastname = req.body.lastname;
+  provider.position = req.body.position;
+  provider.company.company_name = req.body.company_name;
+  provider.company.address = req.body.address;
+  provider.company.address2 = req.body.address2;
+  provider.company.city = req.body.city;
+  provider.company.state = req.body.state;
+  provider.company.postal_code = req.body.postal_code;
+  provider.company.phone = req.body.phone;
+  provider.company.email = req.body.email;
+  provider.company.description = req.body.description;
+  provider.company.tagline = req.body.tagline;
+
   res.render("providers/providers-update", {
     // id: id,
     title: "Service Providers Update",
+    // provider: provider,
+  });
+};
+
+//ADD FORM
+module.exports.addform = function (req, res) {
+  // let id = req.params.id;
+  // let provider = providers.find((provider) => provider.id == id);
+  res.render("providers/providers-add-form", {
+    // id: id,
+    title: "Service Providers Add",
+    // provider: provider,
+  });
+};
+//ADD PROVIDER
+module.exports.add = function (req, res) {
+  //create a random id
+  let min = 100000;
+  let max = 999999;
+  let id = Math.floor(Math.random() * (max - min) + min);
+  // let provider = providers.find((provider) => provider.id == id);
+  // Craete new provider obj
+  let provider = {
+    id: id,
+    firstname: req.body.firstname,
+    lastname: req.body.lastname,
+    position: req.body.position,
+    company: {
+      company_name: req.body.company_name,
+      address: req.body.address,
+      address2: req.body.address2,
+      city: req.body.city,
+      state: req.body.state,
+      postal_code: req.body.postal_code,
+      phone: req.body.phone,
+      email: req.body.email,
+      description: req.body.description,
+      tagline: req.body.tagline,
+    },
+  };
+  //Add provider to list
+  provider.push(provider);
+
+  res.render("providers/providers-add", {
+    // id: id,
+    title: "Service Providers Added",
     // provider: provider,
   });
 };
--- Coursera MEAN/Back-end/routes/providers.js
+++ Coursera MEAN/Back-end/routes/providers.js
@@ -10,4 +10,10 @@ router.get("/details/:id", providerController.details);
 router.get("/edit/:id", providerController.edit);
 /* POST Update Page */
 router.post("/update/:id", providerController.update);
+
+/* GET ADD-PROVIDER Page */
+router.get("/add-provider", providerController.addform);
+/* POST ADD Page */
+router.post("/add", providerController.add);
+
 module.exports = router;
